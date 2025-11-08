112d35229ee0311dfa1e9213d6405fd29e37a094
LucileDesfour <lucedfr@gmail.com>
2025-11-08 13:57:18 +0100

    dichotomique with random between available

--- dichotomous_algorithm/affect_iet.py
+++ dichotomous_algorithm/affect_iet.py
@@ -7,6 +7,12 @@
 import csv
 import random
 
+import sys
+
+log_file = open("results.log","w")
+sys.stdout = log_file
+
+postes = []
 
 with open('affectation.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=';')  
@@ -17,36 +23,14 @@ with open('affectation.csv', 'r') as csvfile:
 
     for row in reader:
         poste = row[0]
+        postes.append(poste)
         for i, student in enumerate(students):
             if row[i+1] == '1':
                 student_choices[student].append(poste)
         random.shuffle(student_choices[student])
 
-
-print(student_choices)
 # je devrais avoir une liste avec {student : [choice1, choice2, choice3]}
-
-# Ordonner dans un ordre aléatoire les étudiants
-
-# Recuperer la liste de poste dans poste_dispo.csv 
-# Dupliquer les postes en fonction du nombre de poste dispo 
-
-with open('poste_dispo.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')  
-    postes_dispo = {}
-
-    next(reader)
-    for row in reader:
-        print(row)
-        postes_dispo[row[0]] = int(row[1])
-
-
-postes = []
-
-for poste, nombre in postes_dispo.items():
-    while nombre > 0:
-        postes.append(poste)        
-        nombre -= 1
+print(student_choices)
 
 random.shuffle(postes)
 random.shuffle(students)
@@ -57,19 +41,18 @@ print(students)
 results = {}
 
 for student in students:
-    print(f"choosing for student {student} : between choices {student_choices[student]} and available {postes}")
+    print(f"choosing for student {student} : with favorite choices {student_choices[student]} between available {postes}")
     # Je parcours les choix des etudiants et si j'arrive a retirer le poste aux postes disponibles je passe a l'etudiant suivant
-    for choice in (student_choices[student]):
-        try:
-            postes.remove(choice)
-            results[student] = choice;
-            students.remove(student)
-            print(f"Student {student} has been assigned to {choice}")
-            break;
-        except ValueError:
-            continue;
+    if not any(choice in postes for choice in student_choices[student]):
+        print(f"No available choices for student {student} among remaining postes")
+        continue
+    else:
+        results[student] = postes.pop()
+        print(f"Student {student} has been assigned to {results[student]}")
+        students.remove(student)
 
-print(f"Les etudiants suivants n'ont eu aucun de leurs choix : {students}")
+
+print(f"Les etudiants n'ont pas eu d'affectations : {students}")
 
 print(f"Nouvelle itéaration avec les postes restants : {postes}")
 for student in students:
@@ -78,3 +61,11 @@ for student in students:
     print(f"Student {student} has been assigned to {available}")
 
 print(f"Results are : {results}")
+
+with open('result_affectation.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(['Student', 'Assigned Poste'])
+    for student, poste in results.items():
+        writer.writerow([student, poste])
+
+log_file.close()
