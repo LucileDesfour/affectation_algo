96e012ac6d94350cf549df8954758f9433246b64
DESFOUR <lucile.desfour@travail.gouv.fr>
2025-11-08 12:59:01 +0100

    start moderatoir choice app

---
+++ moderator_choice/choose_moderator.py
@@ -0,0 +1,27 @@
+Nom;Volontaire;Genre;Groupes;
+Lucile Desfour;1;F;2,3;
+Paul-eloi Rolland;0;M;3;
+Juliette Labois;1;M;2,1;
+Alice Krystofik;0;F;1,2,3;
+Alan Serafin;1;M;1,3;
+Raphael Rigaud;0;M;2,3;
+
+# Je lis le fichier csv IET.csv qui est formaté comme dessus 
+
+Sous_groupe;Groupe;nb_poste;
+Occitanie;1;19;
+ARA-Bourgogne-FC;1;24;
+Normandie;1;10;
+Bretagne;2;9;
+Nouvelle-Aquitaine;2;15;
+Hauts-de-France;2;13;
+Provence-Alpes-Cote-d'azur;2;12;
+Centre-Val-De-Loire Pays de la loire;3;17;
+Grand Est;3;11;
+Ile de France;3;28;
+
+# Les sous groupes contenus dans chaque groupes sont décrits dans ce fichier
+
+
+
+# 
